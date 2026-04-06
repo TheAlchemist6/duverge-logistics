c2a99e69368155a21329d73b5d70a070abc53126
Alchemist6 <gunnarx9@gmail.com>
2026-04-06 16:27:04 -0400

    Update logo and increase hero logo size

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -121,12 +121,12 @@ export default function Hero() {
           >
             {/* Logo Display */}
             <div className="relative mb-8 flex justify-center">
-              <div className="relative w-64 h-64 lg:w-80 lg:h-80">
+              <div className="relative w-80 h-80 lg:w-[420px] lg:h-[420px]">
                 <Image
                   src="/logo.png"
                   alt="Duverge Logistics"
                   fill
-                  className="object-contain drop-shadow-[0_0_60px_rgba(14,165,233,0.3)]"
+                  className="object-contain drop-shadow-[0_0_80px_rgba(14,165,233,0.4)]"
                   priority
                 />
               </div>
